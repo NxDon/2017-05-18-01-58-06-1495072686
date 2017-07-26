b1b23db6d64f3e78fa8e343fdeec45c8ee539a1b
NxDon <nxduanyu@gmail.com>
2017-07-26 22:39:12 +0800

    finish request

--- main/main.js
+++ main/main.js
@@ -1,3 +1,9 @@
-module.exports = function main() {
+module.exports = function main(str) {
   // Write your cade here
+  var strarr = str.split("");
+  var sum = 0;
+  for(var i in strarr){
+  	sum += parseInt(strarr[i]);
+  }
+  return sum;
 };
